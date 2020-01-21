af7fb38c5536c96cf1e8ce7e7d9d22ed86b76fdc
Diogo Azevedo <a21702801@alunos.ulht.pt>
2020-01-21 15:44:43 +0000

    yup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListPersonsComponent } from '../list-persons/list-persons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPersonsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [ListPersonsComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatButtonModule, MatInputModule} from '@angular/material';
+
+@NgModule({
+
+    declarations: [],
+
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
+
+  exports: [
+    MatButtonModule,
+    MatInputModule
+  ]
+})
+export class SharedModule {}
